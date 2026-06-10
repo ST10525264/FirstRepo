055ab46bd81362fd5183be1f8457188d3fff58fc
ST10525264 <ST10525264@myemeris.edu.za>
2026-06-10 12:25:58 +0200

    added : errors

--- app/src/main/java/com/example/campsitecommander/MainActivity.kt
+++ app/src/main/java/com/example/campsitecommander/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.campsitecommander
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.transition.AutoTransition
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.`splashscreen.xml`)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            if(splashScreen.apply(android.R.transition)) {
+                android.transition.AutoTransition "mainscreen"
+            }
         }
     }
 }
